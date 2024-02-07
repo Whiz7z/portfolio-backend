cae078982ce7593b1070dd2a6dfa40b8cd12d66d
Whiz7z <gjevstratov@gmail.com>
2024-02-07 21:54:44 +0000

    ...

---
+++ src/api/project-skill/routes/project-skill.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * project-skill router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::project-skill.project-skill');
